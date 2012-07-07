71efea59fa9ab88440a311749731c65ffaf78d3f
Zarino Zappia <mail@zarino.co.uk>
2012-07-07 21:29:34 +0100

    a few more endpoints

--- endpoints.php
+++ endpoints.php
@@ -14,6 +14,21 @@ function items(){
 	pretty_print_r($_SERVER);
 }
 
+function item(){
+	print 'You requested a single item page<br/><br/>';
+	pretty_print_r($_SERVER);
+}
+
+function reminders(){
+	print 'You requested the reminders page<br/><br/>';
+	pretty_print_r($_SERVER);
+}
+
+function reminder(){
+	print 'You requested a single reminder page<br/><br/>';
+	pretty_print_r($_SERVER);
+}
+
 function fourohfour(){
 	print 'You requested something we don&rsquo;t have &ndash; 404!<br/><br/>';
 	pretty_print_r($_SERVER);
