b7a44bbb12de0a8379375e71dce1b7790ee2b49e
Zarino Zappia <mail@zarino.co.uk>
2012-07-11 07:36:17 +0100

    better pretty_print_r() function - now takes a prefix and suffix

--- functions.php
+++ functions.php
@@ -11,10 +11,24 @@ if($_SERVER['REQUEST_METHOD'] == 'PUT') {
 	$GLOBALS['_DELETE'] = $post_vars;
 }
 
-function pretty_print_r($array){
-	print '<pre>';
-	print_r($array);
-	print '</pre>';
+function pretty_print_r($arg1, $arg2='', $suffix=''){
+	if(is_array($arg1) || is_object($arg1)){
+		$array = $arg1;
+	} else if(is_array($arg2) || is_object($arg2)) {
+		$array = $arg2;
+	}
+	if(is_string($arg1)){
+		$prefix = $arg1;
+	} else if(is_string($arg2)) {
+		$prefix = $arg2;
+	}
+	if(isset($prefix) && isset($array)){
+		print $prefix . '<pre>';
+		print_r($array);
+		print '</pre>' . $suffix;
+	} else {
+	    trigger_error("Incorrect parameters supplied to pretty_print_r() - function takes an optional string prefix, a required array, and an optional string suffix", E_USER_ERROR);
+	}
 }
 
 ?>
