485972f2004b5ac2509d8cf56b0e37b021ee203b
Zarino Zappia <mail@zarino.co.uk>
2012-07-11 07:38:53 +0100

    items() endpoint now differentiates between GET and POST requests, and returns a 405 HTTP error if another request method is used.

--- endpoints.php
+++ endpoints.php
@@ -5,34 +5,55 @@
 //	These are called by index.php, and found via urls.php
 
 function homepage(){
-	print 'You requested the homepage<br/><br/>';
+	print 'You requested the homepage';
 	pretty_print_r($_SERVER);
 }
 
 function items(){
-	print 'You requested the items page<br/><br/>';
-	pretty_print_r($_SERVER);
+	if($_SERVER['REQUEST_METHOD'] == 'GET'){
+		if(empty($_SERVER['QUERY_STRING'])){
+			print 'You requested a list of all items';
+		} else {
+			print 'You just tried to search the items';
+		}
+	} else if($_SERVER['REQUEST_METHOD'] == 'POST'){
+		print 'You just tried to create a new item';
+	} else {
+		header($_SERVER["SERVER_PROTOCOL"] . " 405 Method Not Allowed");
+		print 'This endpoint only accepts GET and POST requests, to list all items and to add a new item, respectively.';
+	}
+	pretty_print_r('<br/><br/>', $_SERVER);
 }
 
 function item(){
-	print 'You requested a single item page<br/><br/>';
-	pretty_print_r($_SERVER);
+	print 'You requested a single item page';
+	pretty_print_r('<br/><br/>', $_SERVER);
 }
 
 function reminders(){
-	print 'You requested the reminders page<br/><br/>';
-	pretty_print_r($_SERVER);
+	print 'You requested the reminders page';
+	pretty_print_r('<br/><br/>', $_SERVER);
 }
 
 function reminder(){
-	print 'You requested a single reminder page<br/><br/>';
-	pretty_print_r($_SERVER);
+	print 'You requested a single reminder page';
+	pretty_print_r('<br/><br/>', $_SERVER);
+}
+
+function updates(){
+	print 'You requested the updates page';
+	pretty_print_r('<br/><br/>', $_SERVER);
+}
+
+function settings(){
+	print 'You requested the settings page';
+	pretty_print_r('<br/><br/>', $_SERVER);
 }
 
 function fourohfour(){
 	header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
-	print 'You requested something we don&rsquo;t have &ndash; 404!<br/><br/>';
-	pretty_print_r($_SERVER);
+	print 'You requested something we don&rsquo;t have &ndash; 404!';
+	pretty_print_r('<br/><br/>', $_SERVER);
 }
 
 ?>
