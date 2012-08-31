f9169444d162688aef20ac4ea7bb6b6404ee35c0
Zarino Zappia <mail@zarino.co.uk>
2012-08-31 17:13:27 +0100

    no need for dictionary in $urls - list will do just fine

--- urls.php
+++ urls.php
@@ -2,17 +2,17 @@
 
 //	If you've used Django, this will look familiar.
 //	We define mappings between REQUEST_URIs and functions from endpoints.php
-//	'<pairing_name>' => array('<uri_regexp>','<endpoint_function>')
+//	array('<uri_regexp>','<endpoint_function>')
 	
 //	Hash symbols '#' in the <uri_regexp> must be escaped.
 
 $urls = array(
-	'homepage' => array('^/$','homepage'),
-	'items' => array('^/items(/|\.html|\.json)?(\?.+)?$','items'),
-	'item' => array('^/items/[a-zA-Z0-9]+(/|\.html|\.json)?(\?.+)?$','item'),
-	'reminders' => array('^/reminders(/|\.html|\.json)?(\?.+)?$','reminders'),
-	'reminder' => array('^/reminder/[a-zA-Z0-9]+(/|\.html|\.json)?(\?.+)?$','reminder'),
-	'fourohfour' => array('.','fourohfour')
+	array('^/$','homepage'),
+	array('^/items(/|\.html|\.json)?(\?.+)?$','items'),
+	array('^/items/[a-zA-Z0-9]+(/|\.html|\.json)?(\?.+)?$','item'),
+	array('^/reminders(/|\.html|\.json)?(\?.+)?$','reminders'),
+	array('^/reminder/[a-zA-Z0-9]+(/|\.html|\.json)?(\?.+)?$','reminder'),
+	array('.','fourohfour')
 );
 
 ?>
