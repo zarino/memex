d0a75c0b2784ad6b256510a92564c53610903765
Zarino Zappia <mail@zarino.co.uk>
2013-05-24 20:35:51 +0100

    less ambiguous variable name

--- api/index.php
+++ api/index.php
@@ -11,9 +11,9 @@ connect_to_database();
 
 $resp = new Response();
 
-foreach($urls as $u){
-    if(preg_match('#'.$u[0].'#', $_SERVER['REQUEST_URI'])){
-        $u[1]();
+foreach($urls as $url){
+    if(preg_match('#'.$url[0].'#', $_SERVER['REQUEST_URI'])){
+        $url[1]();
         break;
     }
 }
