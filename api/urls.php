9887d31112dc243a435599443775c1fec71aea31
Zarino Zappia <mail@zarino.co.uk>
2013-05-24 20:24:24 +0100

    move REST API into subdirectory

--- api/urls.php
+++ api/urls.php
@@ -7,11 +7,11 @@
 //  Hash symbols '#' in the <uri_regexp> must be escaped.
 
 $urls = array(
-    array('^/$','homepage'),
-    array('^/items/?(\?.+)?$','items'),
-    array('^/items/[a-zA-Z0-9]+/?(\?.+)?$','item'),
-    array('^/reminders/?(\?.+)?$','reminders'),
-    array('^/reminder/[a-zA-Z0-9]+/?(\?.+)?$','reminder'),
+    array('^/api/$','homepage'),
+    array('^/api/items/?(\?.+)?$','items'),
+    array('^/api/items/[a-zA-Z0-9]+/?(\?.+)?$','item'),
+    array('^/api/reminders/?(\?.+)?$','reminders'),
+    array('^/api/reminder/[a-zA-Z0-9]+/?(\?.+)?$','reminder'),
     array('.','fourohfour')
 );
 
